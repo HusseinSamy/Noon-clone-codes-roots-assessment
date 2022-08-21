9857f192e4a0bd6e9efb52a1f831e88a44ba8fd0
Hussein Samy <hussein.samy02@gmail.com>
2022-08-21 23:52:12 +0200

    Adds tailwindscss configuration

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "790px",
+      lg: "1120px",
+      xl: "1410px",
+      "2xl": "1536px",
+    },
+    colors: {
+      yellow: "#f1c40f",
+      background_color: "#f7f7fa",
+
+    },
+    fontFamily: {
+      cairo: ["Cairo"],
+    },
+    extend: {
+    },
+    plugins: [
+    ]
+  },
+};
