55092fac17b82a5d682068422e613d33fc528223
Hussein Samy <hussein.samy02@gmail.com>
2022-08-22 05:23:33 +0200

    Updates tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     screens: {
+      xs: "460px",
       sm: "640px",
       md: "790px",
       lg: "1120px",
-      xl: "1410px",
+      xl: "1440px",
       "2xl": "1536px",
     },
     colors: {
-      yellow: "#f1c40f",
+      yellow: "#fef253",
       background_color: "#f7f7fa",
 
     },
