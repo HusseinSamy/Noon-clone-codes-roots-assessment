0935667eaf440eef039e5bcc8b60acfd988f38a3
Hussein Samy <hussein.samy02@gmail.com>
2022-08-22 05:22:15 +0200

    Creates categories component

---
+++ src/app/categories-carousel/categories-carousel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { SwiperService } from 'src/_services/swiper.service';
+
+@Component({
+  selector: 'app-categories-carousel',
+  templateUrl: './categories-carousel.component.html',
+  styleUrls: ['./categories-carousel.component.css']
+})
+export class CategoriesCarouselComponent implements OnInit {
+
+  constructor(private swiper: SwiperService){}
+  config = this.swiper.config2;
+  ngOnInit(): void {
+  }
+
+}
