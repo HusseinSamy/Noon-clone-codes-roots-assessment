97f0b3e55a5090b4f0b24f406d9bed4f85e0b5eb
Hussein Samy <hussein.samy02@gmail.com>
2022-08-23 04:33:20 +0200

    Adds some tests to the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/_services/data.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private data: DataService) {
+  }
+  response: Object = {};
+  baseUrl: string = `http://satafood.codesroots.com:3000/api`;
+  homePageEndpoint: string = `/MobileMainPage/GetHomePage`;
+  mainSliderEndpoint: string = `/MobileMainPage/GetMainSliders`;
+
+    mainMenuData() {
+      this.data.getMainMenu(this.baseUrl + this.homePageEndpoint).subscribe({
+      next: (data) => {
+        this.response = data;
+        console.log('i have passed the next test');
+        console.log(data);
+
+      },
+      error: (err) => {
+        console.log('i am stuck in the error section');
+        return err;
+      }
+    });
+  }
+  ngOnInit(): void {
+  }
+}
