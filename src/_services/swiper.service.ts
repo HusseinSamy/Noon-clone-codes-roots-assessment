7715184ddc470c84cf60bde02da6c149ac71ff7e
Hussein Samy <hussein.samy02@gmail.com>
2022-08-24 22:37:12 +0200

    Adds more configurations to swiper

--- src/_services/swiper.service.ts
+++ src/_services/swiper.service.ts
@@ -26,13 +26,48 @@ export class SwiperService {
             disableOnInteraction: true,
             reverseDirection: true,
         },
+        
     };
     config2: SwiperOptions = {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        navigation: true,
-        scrollbar: { draggable: true },
-        autoplay: false,
+      loop:false,
+      navigation: true,
+      scrollbar: { draggable: true },
+      loopFillGroupWithBlank:false,
+      pagination:{
+        clickable: true
+      },
+      breakpoints: {
+        350: {
+          slidesPerView:2,
+          spaceBetween:2,
+          slidesPerGroup:2,
+        },
+        460: {
+          slidesPerView:3,
+          spaceBetween:5,
+          slidesPerGroup:3,
+        },
+        640: {
+          slidesPerView:4,
+          spaceBetween:7,
+          slidesPerGroup:4,
+        }, 
+        790: {
+          slidesPerView:5,
+          spaceBetween:10,
+          slidesPerGroup:5,
+        }, 
+        1120: {
+          slidesPerView:7,
+          spaceBetween:10,
+          slidesPerGroup:7,
+        },
+        1440: {
+          slidesPerView:8,
+          spaceBetween:10,
+          slidesPerGroup:8,
+        }
+      }
     };
     ngOnInit(): void {}
 }
