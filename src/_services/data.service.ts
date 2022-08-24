d539e6a3cc53879dcc6f440d97457f9b01393da9
Hussein Samy <hussein.samy02@gmail.com>
2022-08-24 05:05:05 +0200

    Creates post and get methods

--- src/_services/data.service.ts
+++ src/_services/data.service.ts
@@ -2,15 +2,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class DataService {
+    constructor(private http: HttpClient) {}
+    baseUrl: string = `http://satafood.codesroots.com:3000/api`;
+    subRequestUrl: string = `http://satafood.codesroots.com:3000`;
+    mainSliderEndpoint: string = `/MobileMainPage/GetMainSliders`;
 
-  constructor(private http: HttpClient) { }
-
-  header: HttpHeaders =  new HttpHeaders({'lang': 'en'});
-  body = {"googleId" : "ChIJ88rv8bI_WBQRkvVBLDeZQUg"}
-  getMainMenu(url: string) {
-    return this.http.post(url,this.body,{headers: this.header});
+    header: HttpHeaders = new HttpHeaders({ lang: 'en' });
+    body = { googleId: 'ChIJ88rv8bI_WBQRkvVBLDeZQUg' };
+    PostEndpoints(url: string) {
+        return this.http.post(url, this.body, { headers: this.header });
+    }
+    GetEndpoints(url: string) {
+        return this.http.get<any>(url);
     }
 }
