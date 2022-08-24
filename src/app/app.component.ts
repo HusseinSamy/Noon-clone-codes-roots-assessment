77d9f35ef088b3bb7b5310efe87a068d8eee0b13
Hussein Samy <hussein.samy02@gmail.com>
2022-08-24 05:06:16 +0200

    Removes testing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,7 @@ import { DataService } from 'src/_services/data.service';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor(private data: DataService) {
-  }
-  response: Object = {};
-  baseUrl: string = `http://satafood.codesroots.com:3000/api`;
-  homePageEndpoint: string = `/MobileMainPage/GetHomePage`;
-  mainSliderEndpoint: string = `/MobileMainPage/GetMainSliders`;
+    constructor() {}
 
-    mainMenuData() {
-      this.data.getMainMenu(this.baseUrl + this.homePageEndpoint).subscribe({
-      next: (data) => {
-        this.response = data;
-        console.log('i have passed the next test');
-        console.log(data);
-
-      },
-      error: (err) => {
-        console.log('i am stuck in the error section');
-        return err;
-      }
-    });
-  }
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 }
