d270696699f7d5463aebc32504f15683b5b0ee56
Hussein Samy <hussein.samy02@gmail.com>
2022-08-25 13:39:07 +0200

    Creates sub-header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperService } from 'src/_services/swiper.service';
 
 @Component({
     selector: 'app-header',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-    constructor() {}
-
+    constructor(private swiper: SwiperService) {}
+    config = this.swiper.config3;
+    headerCategories = ["الإلكترونيات","موبايلات","رجالية",
+    "نسائية","المنزل","الجمال و العطور","الاطفال و الالعاب",
+    "سوبر ماركت","بيع في نون"]
     ngOnInit(): void {}
 }
