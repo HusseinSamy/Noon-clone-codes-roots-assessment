ec13d144065ce50c19ccbbc38d355da1e8fc5454
Hussein Samy <hussein.samy02@gmail.com>
2022-08-27 15:58:04 +0200

    Emits an event on dropdown nav bar toggle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { SwiperService } from 'src/_services/swiper.service';
 
 @Component({
@@ -9,7 +9,14 @@ import { SwiperService } from 'src/_services/swiper.service';
 export class HeaderComponent implements OnInit {
     constructor(private swiper: SwiperService) {}
     config = this.swiper.config3;
-    headerCategories = ["بيع في نون","سوبر ماركت","الأطفال و الألعاب","الجمال و العطور","المنزل","نسائية","رجالية","موبايلات","الإلكترونيات","عروض",]
+    headerCategories = ["بيع في نون","سوبر ماركت","الأطفال و الألعاب","الجمال و العطور","المنزل","نسائية","رجالية","موبايلات","الإلكترونيات"]
+    showNavBar: boolean = false;
+    @Output()
+    show: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+    toggleNavBar() {
+      this.showNavBar = !this.showNavBar;
+      this.show.emit(this.showNavBar);
+    }
     ngOnInit(): void {}
 }
