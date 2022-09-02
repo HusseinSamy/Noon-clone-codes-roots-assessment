6f21412be523c3c8b5c73e89df8c2391328fdc01
Hussein Samy <hussein.samy02@gmail.com>
2022-09-02 23:41:37 +0200

    Adds products component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageModule } from './home-page/home-page.module';
+import { ProductsPageComponent } from './products-page/products-page.component';
 
 @NgModule({
     declarations: [
-      AppComponent
+      AppComponent,
+      ProductsPageComponent
     ],
     imports: [
       HomePageModule,
