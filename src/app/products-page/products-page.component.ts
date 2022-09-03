5f4ee38db5ca3fa82bd7476f1c89720ca26228c6
Hussein Samy <hussein.samy02@gmail.com>
2022-09-03 18:29:33 +0200

    Creates a starter code for products page

--- src/app/products-page/products-page.component.ts
+++ src/app/products-page/products-page.component.ts
@@ -6,9 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-page.component.css']
 })
 export class ProductsPageComponent implements OnInit {
-
+  responsiveOptions = [
+    {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+    }
+];
   constructor() { }
-
+  cars = [{
+    name: 'koko',
+    image: 'assets/sales/1.JPG'
+  },{
+    name: 'koko',
+    image: 'assets/sales/1.JPG'
+  },{
+    name: 'koko',
+    image: 'assets/sales/1.JPG'
+  },{
+    name: 'koko',
+    image: 'assets/sales/1.JPG'
+  },{
+    name: 'koko',
+    image: 'assets/sales/1.JPG'
+  },]
   ngOnInit(): void {
   }
 
