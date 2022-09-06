f1b56aac03d798379d2abede27600e420f19c8fd
Hussein Samy <hussein.samy02@gmail.com>
2022-09-06 21:11:03 +0200

    Imports shared module

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,28 +1,28 @@
 import { SwiperModule } from 'swiper/angular';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'primeng/api';
 
 import { NgModule } from "@angular/core";
 import { MainCarouselComponent } from './main-carousel/main-carousel.component';
 import { CategoriesCarouselComponent } from './categories-carousel/categories-carousel.component';
 import { SalesComponent } from './sales/sales.component';
-import { HeaderComponent } from '../header/header.component';
 import { ProductsComponent } from './products/products.component';
-import { NavBarDropdownComponent } from '../nav-bar-dropdown/nav-bar-dropdown.component';
 import { HomePageComponent } from './home-page.component';
-
 
 @NgModule({
   declarations: [
     MainCarouselComponent,
-    HeaderComponent,
     CategoriesCarouselComponent,
     SalesComponent,
     ProductsComponent,
-    NavBarDropdownComponent,
     HomePageComponent
   ],
-  imports: [SwiperModule,CommonModule],
-  exports: [HomePageComponent, HeaderComponent, NavBarDropdownComponent]
+  imports: [
+    SwiperModule,
+    CommonModule,
+    SharedModule
+  ],
+  exports: [HomePageComponent,]
 })
 export class HomePageModule {
 
