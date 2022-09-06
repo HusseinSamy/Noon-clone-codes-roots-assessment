6f44d5e7888c252e7cf3675cdc6149a955bc63c6
Hussein Samy <hussein.samy02@gmail.com>
2022-09-06 05:27:53 +0200

    Adds shorted pipe and ProductsSliderComponent

--- src/app/products-page/products-page.module.ts
+++ src/app/products-page/products-page.module.ts
@@ -2,24 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'primeng/carousel';
 
+
 import { ImgMagnifier } from "ng-img-magnifier";
 import { ProductsPageComponent } from './products-page.component';
 import { SpecificationsComponent } from './specifications/specifications.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductsSliderComponent } from './products-slider/products-slider.component';
 
+import { ShortenPipe } from 'src/_shared/pipes/shorten.pipe';
 
 
 @NgModule({
   declarations: [
     ProductsPageComponent,
     SpecificationsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ProductsSliderComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule,
     ImgMagnifier,
     CarouselModule
   ],
-  exports: [ProductsPageComponent]
+  exports: [ProductsPageComponent, ShortenPipe]
 })
 export class ProductsPageModule { }
