b8dc80117c21112441ee6e52762d47de0c4bd52c
Hussein Samy <hussein.samy02@gmail.com>
2022-09-06 21:46:24 +0200

    Moves shared module inside modules folder

--- src/_shared/modules/shared.module.ts
+++ src/_shared/modules/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
 
 import { HeaderComponent } from 'src/_shared/components/header/header.component';
 import { NavBarDropdownComponent } from 'src/_shared/components/nav-bar-dropdown/nav-bar-dropdown.component';
@@ -10,10 +11,13 @@ import { NavBarDropdownComponent } from 'src/_shared/components/nav-bar-dropdown
     NavBarDropdownComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SwiperModule
   ],
   exports:[
     HeaderComponent,
-    NavBarDropdownComponent]
+    NavBarDropdownComponent,
+    SwiperModule
+  ]
 })
 export class SharedModule { }
