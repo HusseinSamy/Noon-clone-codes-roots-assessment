649790e69cb279e17237def4ee6300e48f6d117a
Hussein Samy <hussein.samy02@gmail.com>
2022-09-08 21:47:26 +0200

    Creates /products path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/product",
+    pathMatch: 'full'
+  },
+  {
+    path: "product",
+    loadChildren: () => import('./products-page/products-page.module').then(m => m.ProductsPageModule)
+  },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
